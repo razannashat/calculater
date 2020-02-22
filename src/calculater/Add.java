83465655a632d5db1d18dd37fa820aeaf6fdb2d0
razan196 <razanyameen22@gmail.com>
2020-02-22 10:25:47 +0200

    project

---
+++ src/calculater/Add.java
@@ -0,0 +1,12 @@
+package calculater;
+
+public class Add {
+	public static int add(int x, int y) {
+		return x+y;}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int z= add(5,3);
+	}
+
+}
